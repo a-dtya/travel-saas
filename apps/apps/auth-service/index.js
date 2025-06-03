dc5413062b6ae74d174b2a649996a66fd26112d7
a-dtya <adithyavinod1943@gmail.com>
2025-06-03 08:31:19 +0530

    updated auth index -w errorMiddleware

--- apps/apps/auth-service/index.js
+++ apps/apps/auth-service/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const port = process.env.PORT || 6001;
+const { errorMiddleware } = require("../../../packages/error-handler");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    exposedHeaders: ["Authorization", "Content-Type"],
+}));
+
+app.get("/", (req, res) => {
+    res.send("Auth Service API");
+});
+
+app.use(errorMiddleware);
+
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}/api`);
 });
+
+app.on("error", (err)=>{
+    console.log("Server error:", err);
+})
